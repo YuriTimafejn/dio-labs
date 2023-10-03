5160d9b700595cb5c74f524f9627441d9cf64d7c
Timafejn, Yuri <timafejn@gmail.com>
2023-10-03 10:42:04 -0300

    Initial commit

---
+++ santander-java-2023/src/main/java/com/timafejn/santanderjava2023/SantanderJava2023Application.java
@@ -0,0 +1,13 @@
+package com.timafejn.santanderjava2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderJava2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderJava2023Application.class, args);
+	}
+
+}
---
+++ santander-java-2023/src/test/java/com/timafejn/santanderjava2023/SantanderJava2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timafejn.santanderjava2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderJava2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
