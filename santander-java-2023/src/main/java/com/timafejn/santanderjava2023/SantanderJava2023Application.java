05173557f43c527ad7d3054b63dc760f67ac58fe
Timafejn, Yuri <timafejn@gmail.com>
2023-10-09 00:04:29 -0300

    Add annotation 'OpenApiDefinnition'

--- santander-java-2023/src/main/java/com/timafejn/santanderjava2023/SantanderJava2023Application.java
+++ santander-java-2023/src/main/java/com/timafejn/santanderjava2023/SantanderJava2023Application.java
@@ -1,9 +1,12 @@
 package com.timafejn.santanderjava2023;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(servers = { @Server( url = "/")})
 public class SantanderJava2023Application {
 
 	public static void main(String[] args) {
