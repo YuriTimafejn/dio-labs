085844c1c05d8ef3c58c66187262a131f972c9b1
Timafejn, Yuri <timafejn@gmail.com>
2023-10-18 10:58:19 -0300

    feat: completed the challenge

--- typescript/desafio01-ts/app.ts
+++ typescript/desafio01-ts/app.ts
@@ -1,9 +1,9 @@
 import { CompanyAccount } from './class/CompanyAccount'
 import { PeopleAccount } from './class/PeopleAccount'
 
-const peopleAccount: PeopleAccount = new PeopleAccount(1, 'Nath', 10)
-console.log(peopleAccount)
-peopleAccount.deposit()
-const companyAccount: CompanyAccount = new CompanyAccount('DIO', 20)
-companyAccount.deposit()
-console.log(companyAccount)
+const peopleAccount: PeopleAccount = new PeopleAccount(1, 'Nath', 10);
+peopleAccount.deposit(100);
+console.log(peopleAccount);
+const companyAccount: CompanyAccount = new CompanyAccount('DIO', 20);
+companyAccount.getLoan(3000);
+console.log(companyAccount);
